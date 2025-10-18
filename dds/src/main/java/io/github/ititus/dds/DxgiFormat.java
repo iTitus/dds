0ea2fc83f933e91c54eb9d4fa1c862d4a2256cd6
iTitus <iTitus@users.noreply.github.com>
2025-10-18 14:42:30 +0200

    add two new constants found in dxgi docs

--- dds/src/main/java/io/github/ititus/dds/DxgiFormat.java
+++ dds/src/main/java/io/github/ititus/dds/DxgiFormat.java
@@ -123,7 +123,9 @@ public enum DxgiFormat implements PixelFormat {
     B4G4R4A4_UNORM(115),
     P208(130),
     V208(131),
-    V408(132);
+    V408(132),
+    SAMPLER_FEEDBACK_MIN_MIP_OPAQUE(189),
+    SAMPLER_FEEDBACK_MIP_REGION_USED_OPAQUE(190);
 
     private static final DxgiFormat[] VALUES = values();
 
@@ -160,42 +162,40 @@ public enum DxgiFormat implements PixelFormat {
         return switch (this) {
             case R32G32B32A32_TYPELESS, R32G32B32A32_FLOAT, R32G32B32A32_UINT, R32G32B32A32_SINT -> 128;
             case R32G32B32_TYPELESS, R32G32B32_FLOAT, R32G32B32_UINT, R32G32B32_SINT -> 96;
-            case R16G16B16A16_TYPELESS, R16G16B16A16_FLOAT, R16G16B16A16_UNORM, R16G16B16A16_UINT, R16G16B16A16_SNORM
-                    , R16G16B16A16_SINT,
-                    R32G32_TYPELESS, R32G32_FLOAT, R32G32_UINT, R32G32_SINT,
-                    R32G8X24_TYPELESS,
-                    D32_FLOAT_S8X24_UINT, R32_FLOAT_X8X24_TYPELESS, X32_TYPELESS_G8X24_UINT,
-                    Y416, Y210, Y216 -> 64;
+            case R16G16B16A16_TYPELESS, R16G16B16A16_FLOAT, R16G16B16A16_UNORM, R16G16B16A16_UINT, R16G16B16A16_SNORM, R16G16B16A16_SINT,
+                 R32G32_TYPELESS, R32G32_FLOAT, R32G32_UINT, R32G32_SINT, R32G8X24_TYPELESS,
+                 D32_FLOAT_S8X24_UINT, R32_FLOAT_X8X24_TYPELESS, X32_TYPELESS_G8X24_UINT,
+                 Y416, Y210, Y216 -> 64;
             case R10G10B10A2_TYPELESS, R10G10B10A2_UNORM, R10G10B10A2_UINT,
-                    R11G11B10_FLOAT,
-                    R8G8B8A8_TYPELESS, R8G8B8A8_UNORM, R8G8B8A8_UNORM_SRGB, R8G8B8A8_UINT, R8G8B8A8_SNORM,
-                    R8G8B8A8_SINT,
-                    R16G16_TYPELESS, R16G16_FLOAT, R16G16_UNORM, R16G16_UINT, R16G16_SNORM, R16G16_SINT,
-                    R32_TYPELESS, D32_FLOAT, R32_FLOAT, R32_UINT, R32_SINT, R24G8_TYPELESS,
-                    D24_UNORM_S8_UINT, R24_UNORM_X8_TYPELESS, X24_TYPELESS_G8_UINT,
-                    R9G9B9E5_SHAREDEXP,
-                    R8G8_B8G8_UNORM, G8R8_G8B8_UNORM,
-                    R10G10B10_XR_BIAS_A2_UNORM,
-                    B8G8R8A8_TYPELESS, B8G8R8A8_UNORM, B8G8R8A8_UNORM_SRGB,
-                    B8G8R8X8_TYPELESS, B8G8R8X8_UNORM, B8G8R8X8_UNORM_SRGB,
-                    AYUV, Y410, YUY2 -> 32;
+                 R11G11B10_FLOAT,
+                 R8G8B8A8_TYPELESS, R8G8B8A8_UNORM, R8G8B8A8_UNORM_SRGB, R8G8B8A8_UINT, R8G8B8A8_SNORM, R8G8B8A8_SINT,
+                 R16G16_TYPELESS, R16G16_FLOAT, R16G16_UNORM, R16G16_UINT, R16G16_SNORM, R16G16_SINT,
+                 R32_TYPELESS, D32_FLOAT, R32_FLOAT, R32_UINT, R32_SINT,
+                 R24G8_TYPELESS,
+                 D24_UNORM_S8_UINT, R24_UNORM_X8_TYPELESS, X24_TYPELESS_G8_UINT,
+                 R9G9B9E5_SHAREDEXP,
+                 R8G8_B8G8_UNORM, G8R8_G8B8_UNORM,
+                 R10G10B10_XR_BIAS_A2_UNORM,
+                 B8G8R8A8_TYPELESS, B8G8R8A8_UNORM, B8G8R8A8_UNORM_SRGB,
+                 B8G8R8X8_TYPELESS, B8G8R8X8_UNORM, B8G8R8X8_UNORM_SRGB,
+                 AYUV, Y410, YUY2 -> 32;
             case P010, P016, V408 -> 24;
             case R8G8_TYPELESS, R8G8_UNORM, R8G8_UINT, R8G8_SNORM, R8G8_SINT,
-                    R16_TYPELESS, R16_FLOAT, R16_UNORM, R16_UINT, R16_SNORM, R16_SINT,
-                    D16_UNORM,
-                    B5G6R5_UNORM, B5G5R5A1_UNORM, B4G4R4A4_UNORM,
-                    P208, V208 -> 16;
+                 R16_TYPELESS, R16_FLOAT, R16_UNORM, R16_UINT, R16_SNORM, R16_SINT,
+                 D16_UNORM,
+                 B5G6R5_UNORM, B5G5R5A1_UNORM, B4G4R4A4_UNORM,
+                 P208, V208 -> 16;
             case NV12, _420_OPAQUE, NV11 -> 12;
             case R8_TYPELESS, R8_UNORM, R8_UINT, R8_SNORM, R8_SINT,
-                    A8_UNORM,
-                    BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
-                    BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
-                    BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
-                    BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
-                    BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB,
-                    AI44, IA44, P8 -> 8;
+                 A8_UNORM,
+                 BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
+                 BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
+                 BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
+                 BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
+                 BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB,
+                 AI44, IA44, P8 -> 8;
             case BC1_TYPELESS, BC1_UNORM, BC1_UNORM_SRGB,
-                    BC4_TYPELESS, BC4_UNORM, BC4_SNORM -> 4;
+                 BC4_TYPELESS, BC4_UNORM, BC4_SNORM -> 4;
             case R1_UNORM -> 1;
             default -> throw new IllegalStateException("unknown bpp for format " + this);
         };
@@ -206,16 +206,16 @@ public enum DxgiFormat implements PixelFormat {
         return switch (this) {
             case R8G8_B8G8_UNORM, G8R8_G8B8_UNORM, YUY2, Y210, Y216 -> 2;
             case BC1_TYPELESS, BC1_UNORM, BC1_UNORM_SRGB,
-                    BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
-                    BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
-                    BC4_TYPELESS, BC4_UNORM, BC4_SNORM,
-                    BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
-                    BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
-                    BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB -> 4;
+                 BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
+                 BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
+                 BC4_TYPELESS, BC4_UNORM, BC4_SNORM,
+                 BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
+                 BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
+                 BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB -> 4;
             case UNKNOWN,
-                    NV12, P010, P016, _420_OPAQUE, NV11,
-                    P208, V208, V408 ->
-                    throw new IllegalStateException("unknown horizontal pixels per block for format " + this);
+                 NV12, P010, P016, _420_OPAQUE, NV11,
+                 P208, V208, V408,
+                 SAMPLER_FEEDBACK_MIN_MIP_OPAQUE, SAMPLER_FEEDBACK_MIP_REGION_USED_OPAQUE -> throw new IllegalStateException("unknown horizontal pixels per block for format " + this);
             default -> 1;
         };
     }
@@ -224,16 +224,16 @@ public enum DxgiFormat implements PixelFormat {
     public int getVerticalPixelsPerBlock() {
         return switch (this) {
             case BC1_TYPELESS, BC1_UNORM, BC1_UNORM_SRGB,
-                    BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
-                    BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
-                    BC4_TYPELESS, BC4_UNORM, BC4_SNORM,
-                    BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
-                    BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
-                    BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB -> 4;
+                 BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
+                 BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
+                 BC4_TYPELESS, BC4_UNORM, BC4_SNORM,
+                 BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
+                 BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
+                 BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB -> 4;
             case UNKNOWN,
-                    NV12, P010, P016, _420_OPAQUE, NV11,
-                    P208, V208, V408 ->
-                    throw new IllegalStateException("unknown vertical pixels per block for format " + this);
+                 NV12, P010, P016, _420_OPAQUE, NV11,
+                 P208, V208, V408,
+                 SAMPLER_FEEDBACK_MIN_MIP_OPAQUE, SAMPLER_FEEDBACK_MIP_REGION_USED_OPAQUE -> throw new IllegalStateException("unknown vertical pixels per block for format " + this);
             default -> 1;
         };
     }
@@ -242,12 +242,12 @@ public enum DxgiFormat implements PixelFormat {
     public boolean isBlockCompressed() {
         return switch (this) {
             case BC1_TYPELESS, BC1_UNORM, BC1_UNORM_SRGB,
-                    BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
-                    BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
-                    BC4_TYPELESS, BC4_UNORM, BC4_SNORM,
-                    BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
-                    BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
-                    BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB -> true;
+                 BC2_TYPELESS, BC2_UNORM, BC2_UNORM_SRGB,
+                 BC3_TYPELESS, BC3_UNORM, BC3_UNORM_SRGB,
+                 BC4_TYPELESS, BC4_UNORM, BC4_SNORM,
+                 BC5_TYPELESS, BC5_UNORM, BC5_SNORM,
+                 BC6H_TYPELESS, BC6H_UF16, BC6H_SF16,
+                 BC7_TYPELESS, BC7_UNORM, BC7_UNORM_SRGB -> true;
             default -> false;
         };
     }
@@ -264,7 +264,7 @@ public enum DxgiFormat implements PixelFormat {
     public boolean isPlanar() {
         return switch (this) {
             case NV12, P010, P016, _420_OPAQUE, NV11,
-                    P208, V208, V408 -> true;
+                 P208, V208, V408 -> true;
             default -> false;
         };
     }
